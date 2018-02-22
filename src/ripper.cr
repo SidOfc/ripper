8baf9950ed420119cc20e9b892b020e21611b046
Sidney Liebrand <sidneyliebrand@gmail.com>
2018-02-22 07:38:19 +0100

    simplified and improved rendering flow

--- src/ripper.cr
+++ src/ripper.cr
@@ -8,70 +8,76 @@ require "./ripper/property"
 module Ripper
   extend self
 
-  @@vars = {} of String => String
-
-  def vars
-    @@vars
-  end
-
-  def var(name)
-    @@vars[name]?
-  end
-
-  def var(name, value)
-    @@vars[name] = value
-  end
-
-  def process_vars(line)
-    line.gsub(/\$[a-z_\-][\w\-]*/i) { |n| var(n) || n }
+  def process_vars(line, vars = {} of String => String)
+    line.gsub(/\$[^\s]+/) { |m|  vars[m]? || "-ripper-missing(#{m})" }
   end
 
   def parse(content)
-    root          = Selector.new "", root: true
-    last_selector = root
+    root    = Selector.new ""
+    parents = [root] of Selector
+    commas  = [] of Selector
+    vars    = {} of String => String
 
     content.split("\n").each do |line|
+      next if line.empty?
+
       case line_type line
       when :selector
-        current = Selector.new process_vars(line)
+        previous = parents.last
+        current  = Selector.new line
 
-        if current.indent > last_selector.indent
-          current.parent = last_selector
-          last_selector.selectors << current
-        elsif current.indent <= last_selector.indent
-          if pls = last_selector.parent
-            loop do
-              if pls.root? || current.indent > pls.indent
-                current.parent = pls
-                pls.selectors << current
-                break
-              end
+        if line.ends_with? ","
+          commas << current
+          next
+        end
 
-              break unless pls = pls.parent
+        if current.indent > previous.indent
+          parents            << current
+          previous.selectors << current
+        else
+          loop do
+            if parents.empty? || current.indent > previous.indent
+              parents            += parents.empty? ? [root, current] : [previous, current]
+              previous.selectors << current
+              break
             end
-          else
-            current.parent = root
-            root.selectors << current
+
+            previous = parents.pop
           end
         end
 
-        last_selector = current
+        if commas.any?
+          current.commas = commas.map do |comma|
+            comma.target       =  current
+            previous.selectors << comma
+            comma.name
+          end
+
+          commas = [] of Selector
+        end
       when :property
-        last_selector.properties << Property.new process_vars(line)
+        parents.last.properties << Property.new process_vars line, vars
       when :variable
-        key, value = line.split(/\s+/).map(&.strip)
-        var key, value
+        key, value = line.strip.split " ", 2
+        vars[key] = value
       end
     end
 
-    root.render
+    root
   end
 
   def line_type(line : String)
-    return :selector if line =~ /^\s*[&@#.]/ || HTML_TAGS.includes? line.strip.split(' ').first.downcase
+    return :selector if line =~ /^\s*[+&@#.]/ || HTML_TAGS.includes?(line.strip.split(' ').first.downcase)
     return :property if line =~ /^\s*[a-z_-][\w\-]*[:\s][^;]+;?/i
-    return :variable if line =~ /^\s*\$[a-z_][\w\-]*\s+[^;]+;?/i
+    return :variable if line =~ /^\s*\$[a-z_-][\w\-]*[:\s][^;]+;?/i
+    return :line
   end
 end
 
-puts Ripper.parse ARGF.gets_to_end
+tree = Ripper.parse ARGF.gets_to_end
+sel  = Ripper::Selector.new ""
+
+# puts sel.expand [".woohoo", ".there &", "+ &"]
+# puts sel.expand [".there", "&&"]
+
+puts tree.render
--- src/ripper/selector.cr
+++ src/ripper/selector.cr
@@ -2,41 +2,64 @@ require "./property"
 
 module Ripper
   class Selector
-    property :line, :name, :properties, :selectors, :parent, :indent
+    property :line, :name, :properties, :selectors, :indent, :target, :commas
 
-    @parent     : (Selector|Nil)
-    @line       : String
-    @indent     : Int32
-    @name       : String
-    @root       : Bool
-    @properties : Array(Property)
-    @selectors  : Array(Selector)
-    @expanded   : String = ""
+    @target     : (Selector|Nil)
+    @name       = ""
+    @indent     = 0
+    @commas     = [] of String
+    @properties = [] of Property
+    @selectors  = [] of Selector
 
-    def initialize(@line, **options)
-      @name       = @line.tr("{", "").strip
-      @parent     = options[:parent]?
-      @root       = options[:root]? ? true : false
+    def initialize(line, **options)
+      @name       = line.tr("{,", "").strip
+      @target     = options[:target]?
       @properties = options[:properties]? || [] of Property
       @selectors  = options[:selectors]?  || [] of Selector
-      @indent     = @line.partition(/^\s*/)[1].size
+      @indent     = line.partition(/^\s*/)[1].size
     end
 
-    def render(locals = {} of String => String)
-      if root? || statement? || properties.none?
-        output = ""
-      else
-        output = render_self locals
+    def target
+      @target || self
+    end
+
+    def render(names = [] of String)
+      targets = names + [name]
+      content = target == self ? target.render_self(targets) : ""
+
+      content + target.selectors.map(&.render(targets).as(String)).join
+    end
+
+    def expand(names = [] of String)
+      selector = ""
+      ending   = ""
+
+      names.each do |name|
+        case name
+        when /^(?:&?\s*\+\s*&|&&)$/
+          selector += " + #{ending.strip}"
+        when /^&/
+          selector += ending = name.tr "&", ""
+        when /&$/
+          ending   = name.tr "&", ""
+          selector = ending + selector.lstrip
+        when /&/
+          selector = name.sub "&", selector.strip
+          ending   = selector.split(" ").last
+        else
+          selector += ending = " #{name}"
+        end
       end
 
-      selectors.each { |sel| output += statement? ? parse_statement : sel.render(locals) }
-      output
+      selector.strip
     end
 
-    def render_self(locals = {} of String => String)
-      output = expand.tr(",", "").gsub(/\$[a-z_-]+[\w\-]*/i) { |n| locals[n[1..-1]]? || n } + " {\n"
+    def render_self(names = [] of String)
+      return "" if target.properties.none? || names.empty?
+      sub    = expand names[0..-2]
+      output = (commas + [names.last]).map { |n| expand(names[0..-2] + [n]) }.join(",\n") + " {\n"
 
-      properties.each do |prop|
+      target.properties.each do |prop|
         prefixed_props, prefixed_vals = prop.with_prefixes
         prefixed_props.each do |prefixed_prop|
           prefixed_vals.each do |prefixed_val|
@@ -46,48 +69,6 @@ module Ripper
       end
 
       output + "}\n"
-    end
-
-    def expand
-      return @expanded unless @expanded.empty?
-
-      return @expanded = name unless tmp_parent = parent
-      return @expanded = name unless tmp_parent = tmp_parent.parent if tmp_parent.statement?
-
-      return @expanded = name if tmp_parent.root?
-      return @expanded = [tmp_parent.expand, "+", tmp_parent.name.tr("&", "").strip].join " " if name == "&&"
-      return @expanded = [tmp_parent.expand, name.tr("&", "")].join if name.starts_with? "&"
-      return @expanded = [name.tr("&", ""), tmp_parent.expand].join if name.ends_with? "&"
-      return @expanded = name.sub "&", tmp_parent.expand            if name.includes? "&"
-
-      @expanded = [tmp_parent.expand, name].join " "
-    end
-
-    def statement?
-      name.starts_with? "@"
-    end
-
-    def parse_statement
-      parts = name.split /\s+/, 4
-
-      case parts.first
-      when "@each"
-        local = parts[1]
-        seq   = parts[3].lstrip('[').rstrip(']').split(/,\s*/)
-        res   = ""
-
-        seq.each do |val|
-          selectors.each { |sel| res += sel.render({local => val}) }
-        end
-
-        return res
-      end
-
-      ""
-    end
-
-    def root?
-      @root
     end
   end
 end
